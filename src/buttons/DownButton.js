4ef3a4a29527f1c18e5509b2e0ea5a52486d8ece
chadmroberts88 <chadmroberts88@gmail.com>
2022-04-26 15:39:03 -0230

    Implementing styled components API.

--- src/buttons/DownButton.js
+++ src/buttons/DownButton.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { BsCaretDownFill } from 'react-icons/bs'
 
 
-function DownButton({ id, handleTiles }) {
+const DownButton = ({ id, handleTiles }) => {
     return (
         <button id={id} className="arrow-button" onClick={() => {
             handleTiles(id);
--- src/buttons/RightButton.js
+++ src/buttons/RightButton.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BsCaretRightFill } from 'react-icons/bs'
 
-function RightButton({ id, handleTiles }) {
+const RightButton = ({ id, handleTiles }) => {
     return (
         <button id={id} className="arrow-button" onClick={() => {
             handleTiles(id);
